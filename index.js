e94a31e8ca27e333b18147178ce8852713b67a34
Ethan <ethan.steip@gmail.com>
2023-01-30 10:22:19 -0500

    finished very basic socket.io chat-app tut

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// initializes the Socket connection.
+// Prints the message to the terminal
+// and emits the message to anyone else using the same connection.
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  // When a user discconnects, print a message to the terminal.
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
